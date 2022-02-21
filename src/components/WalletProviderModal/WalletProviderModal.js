ef2fb71f1a6fc5052089bf8dfe1a41eb50b19e60
SolanaGuru <topwebdev0824@gmail.com>
2022-02-21 02:28:58 +0200

    added the Coin98 and Math wallet

--- src/components/WalletProviderModal/WalletProviderModal.js
+++ src/components/WalletProviderModal/WalletProviderModal.js
@@ -5,8 +5,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import metamaskLogo from '../../assets/img/metamask-fox.svg';
 import walletConnectLogo from '../../assets/img/wallet-connect.svg';
 import coingBaseLogo from '../../assets/img/coinbase_logo.jpeg';
+import coin98Logo from '../../assets/img/coin98.png';
+import mathWalletLogo from '../../assets/img/mathWallet.png';
 import { useWallet } from 'use-wallet';
-
 const useStyles = makeStyles((theme) => ({
 	paper: {
 		position: 'absolute',
@@ -25,14 +26,40 @@ const useStyles = makeStyles((theme) => ({
 
 const WalletProviderModal = ({ open, handleClose }) => {
 	const classes = useStyles();
-	const { account, connect } = useWallet();
-
+	const wallet = useWallet();
+	const { account, connect, ethereum } = useWallet();
+	console.log(wallet, "&^^^^^^^^^^^^^^^^^^^^^^^")
 	useEffect(() => {
 		if (account) {
 			handleClose();
 		}
 	});
-
+	const getProvider = () => {
+		if ("ethereum" in window) {
+			const provider = ethereum;
+			if (provider.isMathWallet) {
+				return provider;
+			}
+		}
+		window.open("https://mathwallet.org/", "_blank");
+	};
+	const walletConnect = async () => {
+		const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+		const account = accounts[0];
+		console.log(account, "========================>web3")
+		// console.log(account,"****************************")
+		// if (web3) {
+		//   if (!window.ethereum) {
+		// 	return;
+		//   }
+		//   else {
+		// 	const res = await window.ethereum.enable();        
+		// 	// if (res.length) {
+		// 	//   setOwnerAddress(res[0]);
+		// 	// }
+		//   }
+		// }
+	}
 	return (
 		<Modal
 			aria-labelledby="connect a wallet"
@@ -64,6 +91,20 @@ const WalletProviderModal = ({ open, handleClose }) => {
 							connect('walletlink');
 						}}
 						title="Coinbase Wallet"
+					/>
+					<WalletCard
+						icon={<img src={coin98Logo} alt="Coin98 logo" style={{ height: 32, borderRadius: 8 }} />}
+						onConnect={() => {
+							connect('injected');
+						}}
+						title="Coin98 Wallet"
+					/>
+					<WalletCard
+						icon={<img src={mathWalletLogo} alt="Math logo" style={{ height: 32, borderRadius: 8 }} />}
+						onConnect={() => {
+							connect('injected');
+						}}
+						title="Math Wallet"
 					/>
 				</List>
 			</div>
